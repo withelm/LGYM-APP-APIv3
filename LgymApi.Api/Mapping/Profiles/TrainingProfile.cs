84db252977a3b0f7d856eea52b64fb4e38b207e0
withelm <withelm@gmail.com>
2026-02-21 00:42:35 +0100

    [#102] refactor API profiles to reuse nested mapper composition

--- LgymApi.Api/Mapping/Profiles/TrainingProfile.cs
+++ LgymApi.Api/Mapping/Profiles/TrainingProfile.cs
@@ -9,18 +9,14 @@ public sealed class TrainingProfile : IMappingProfile
 {
     public void Configure(MappingConfiguration configuration)
     {
-        configuration.CreateMap<Training, LastTrainingInfoDto>((source, _) => new LastTrainingInfoDto
+        configuration.CreateMap<Training, LastTrainingInfoDto>((source, context) => new LastTrainingInfoDto
         {
             Id = source.Id.ToString(),
             TypePlanDayId = source.TypePlanDayId.ToString(),
             CreatedAt = source.CreatedAt.UtcDateTime,
             PlanDay = source.PlanDay == null
                 ? new PlanDayChooseDto()
-                : new PlanDayChooseDto
-                {
-                    Id = source.PlanDay.Id.ToString(),
-                    Name = source.PlanDay.Name
-                }
+                : context!.Map<PlanDay, PlanDayChooseDto>(source.PlanDay)
         });
     }
 }
