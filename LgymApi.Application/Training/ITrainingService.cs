1bf068883e8a01d3661830ffb500d2b58edaaead
withelm <withelm@gmail.com>
2026-03-23 18:59:18 +0100

    refactor: use AddTrainingInput for training creation

--- LgymApi.Application/Training/ITrainingService.cs
+++ LgymApi.Application/Training/ITrainingService.cs
@@ -5,7 +5,7 @@ namespace LgymApi.Application.Features.Training;
 
 public interface ITrainingService
 {
-    Task<TrainingSummaryResult> AddTrainingAsync(Guid userId, Guid gymId, Guid planDayId, DateTime createdAt, IReadOnlyCollection<TrainingExerciseInput> exercises, CancellationToken cancellationToken = default);
+    Task<TrainingSummaryResult> AddTrainingAsync(Guid userId, AddTrainingInput input, CancellationToken cancellationToken = default);
     Task<TrainingEntity> GetLastTrainingAsync(Guid userId, CancellationToken cancellationToken = default);
     Task<List<TrainingByDateDetails>> GetTrainingByDateAsync(Guid userId, DateTime createdAt, CancellationToken cancellationToken = default);
     Task<List<DateTime>> GetTrainingDatesAsync(Guid userId, CancellationToken cancellationToken = default);
