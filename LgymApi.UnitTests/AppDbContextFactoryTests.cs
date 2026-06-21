5438f533f77e1c597c346974529a2200edbd6f50
Jakub Leśkiewicz <kubaxl222@gmail.com>
2026-06-21 14:09:56 +0200

    test(infra): cover design-time db context factory
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ LgymApi.UnitTests/AppDbContextFactoryTests.cs
@@ -0,0 +1,48 @@
+using FluentAssertions;
+using LgymApi.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace LgymApi.UnitTests;
+
+[TestFixture]
+public sealed class AppDbContextFactoryTests
+{
+    private const string EnvironmentVariableName = "ConnectionStrings__Postgres";
+    private string? _originalValue;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _originalValue = Environment.GetEnvironmentVariable(EnvironmentVariableName);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Environment.SetEnvironmentVariable(EnvironmentVariableName, _originalValue);
+    }
+
+    [Test]
+    public void CreateDbContext_WhenEnvironmentVariableMissing_UsesDefaultConnectionString()
+    {
+        Environment.SetEnvironmentVariable(EnvironmentVariableName, null);
+        var factory = new AppDbContextFactory();
+
+        using var context = factory.CreateDbContext([]);
+
+        context.Database.GetConnectionString().Should().Contain("Host=localhost;Port=5433;Database=LGYM-APP");
+    }
+
+    [Test]
+    public void CreateDbContext_WhenEnvironmentVariableProvided_UsesOverride()
+    {
+        const string connectionString = "Host=prod;Port=5432;Database=LGYM;Username=test;Password=secret";
+        Environment.SetEnvironmentVariable(EnvironmentVariableName, connectionString);
+        var factory = new AppDbContextFactory();
+
+        using var context = factory.CreateDbContext([]);
+
+        context.Database.GetConnectionString().Should().Be(connectionString);
+    }
+}
