a417865116515b14f1d014f878334e779b49b65f
withelm <withelm@gmail.com>
2026-07-05 18:42:16 +0200

    Fix EF translation for typed ID ordering
    Avoid calling Id<T>.GetValue() inside LINQ-to-Entities ordering because EF Core cannot translate custom value-object methods to SQL.
    
    Order by mapped typed-ID properties directly and make typed IDs comparable for deterministic non-relational ordering.

--- LgymApi.Domain/ValueObjects/Id.cs
+++ LgymApi.Domain/ValueObjects/Id.cs
@@ -1,6 +1,6 @@
 namespace LgymApi.Domain.ValueObjects;
 
-public readonly record struct Id<TEntity>
+public readonly record struct Id<TEntity> : IComparable<Id<TEntity>>, IComparable
 {
     public Guid Value { get; }
 
@@ -32,6 +32,23 @@ public readonly record struct Id<TEntity>
     public Guid GetValue() => Value;
 
     public int CompareByValue(Id<TEntity> other) => Value.CompareTo(other.Value);
+
+    public int CompareTo(Id<TEntity> other) => CompareByValue(other);
+
+    public int CompareTo(object? obj)
+    {
+        if (obj is null)
+        {
+            return 1;
+        }
+
+        if (obj is Id<TEntity> other)
+        {
+            return CompareTo(other);
+        }
+
+        throw new ArgumentException($"Object must be of type {typeof(Id<TEntity>)}.", nameof(obj));
+    }
 
     public Id<TScope> Rebind<TScope>() => new(Value);
 
