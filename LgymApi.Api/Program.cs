549255fcc7c345e4c8d02614b4eef31eea2dd5e3
withelm <withelm@gmail.com>
2026-07-11 15:12:21 +0200

    feat(logging): add private log panel (ES+Kibana+nginx auth), Serilog shipping, redaction, all-in-one image and publish workflow (#348)
    ES+Kibana loopback-only stack; nginx basic-auth+TLS+rate-limit gate; Elastic.Serilog.Sinks shipping .NET logs to data stream logs-lgym-app with PII/secret redaction; all-in-one lgym-logpanel image (nginx auth + ES + Kibana via supervisord); logpanel-image.yml GH publish workflow with automatic semver bump; local Docker demo.

--- LgymApi.Api/Program.cs
+++ LgymApi.Api/Program.cs
@@ -13,6 +13,8 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Globalization;
+using Elastic.Ingest.Elasticsearch;
+using Elastic.Serilog.Sinks;
 using LgymApi.Api;
 using LgymApi.Api.Configuration;
 using Microsoft.AspNetCore.Localization;
@@ -21,6 +23,7 @@ using LgymApi.Domain.Security;
 using LgymApi.Api.Constants;
 using Hangfire;
 using LgymApi.Api.Serialization;
+using LgymApi.Api.Logging;
 using LgymApi.BackgroundWorker.Common.Serialization;
 using LgymApi.BackgroundWorker.Common.Jobs;
 using LgymApi.BackgroundWorker.Common.Notifications.Models;
@@ -33,6 +36,8 @@ using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.Http.Json;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Debugging;
 
 const string TestingEnvironment = "Testing";
 
@@ -43,6 +48,26 @@ ExternalConfigBootstrap.Configure(
     builder.Environment.ContentRootPath,
     builder.Environment.EnvironmentName,
     args);
+
+SelfLog.Enable(msg => Console.Error.WriteLine(msg));
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .Enrich.WithProperty("Application", "lgym-api")
+    .Enrich.WithProperty("Environment", builder.Environment.EnvironmentName)
+    .Enrich.With<SensitiveDataEnricher>()
+    .WriteTo.Async(a => a.Elasticsearch(
+        new[] { new Uri(builder.Configuration["Elasticsearch:Url"] ?? "http://host.docker.internal:9200") },
+        opts =>
+        {
+            opts.BootstrapMethod = BootstrapMethod.Silent;
+            opts.DataStream = new Elastic.Ingest.Elasticsearch.DataStreams.DataStreamName("logs", "lgym", "app");
+        }))
+    .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog();
+builder.Services.AddSerilog();
 
 builder.Services
     .AddControllers()
@@ -249,6 +274,7 @@ localizationOptions.RequestCultureProviders = new List<IRequestCultureProvider>
 };
 
 var app = builder.Build();
+
 await StartupMigrationBootstrap.ApplyAsync(app, TestingEnvironment);
 
 if (!app.Environment.IsEnvironment(TestingEnvironment))
@@ -267,6 +293,7 @@ if (app.Environment.IsDevelopment())
 ProgramHangfire.ConfigureRecurringJobs(app, TestingEnvironment);
 
 app.UseRequestLocalization(localizationOptions);
+app.UseSerilogRequestLogging();
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseCors();
 app.UseAuthentication();
